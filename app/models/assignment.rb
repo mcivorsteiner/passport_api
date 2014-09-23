040f421eec0d8976585be86b9c1b76f8d49991b9
McIvor Steiner <mcivorsteiner@gmail.com>
2014-09-23 15:55:14 -0700

    added availability and other methods to Timeslot and Assignment models

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,8 @@ class Assignment < ActiveRecord::Base
   belongs_to :boat
   belongs_to :timeslot
   has_many :bookings
+
+  def availability
+    boat.capacity - bookings.sum(:group_size)
+  end
 end
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,4 +1,16 @@
 class Timeslot < ActiveRecord::Base
   has_many :assignments
   has_many :boats, through: :assignments
+
+  def boat_availabilities
+    assignments.map { |assignment| assignment.availability }
+  end
+
+  def max_availability
+    boat_availabilities.max
+  end
+
+  def boat_ids
+    boats.map { |boat| boat.id }
+  end
 end
