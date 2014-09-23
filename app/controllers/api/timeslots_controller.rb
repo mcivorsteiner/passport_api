20724b4e5790234171bd760ba74a8475ee1d95f2
McIvor Steiner <mcivorsteiner@gmail.com>
2014-09-23 15:11:06 -0700

    created logic for timeslot create endpoint

--- app/controllers/api/timeslots_controller.rb
+++ app/controllers/api/timeslots_controller.rb
@@ -1,4 +1,26 @@
 module Api
   class TimeslotsController < ApplicationController
+    def create
+      @timeslot = Timeslot.new(timeslot_params)
+      if @timeslot.save
+        render :json => { id: @timeslot.id, start_time: @timeslot.start_time.to_time.to_i, duration: @timeslot.duration, availability: 0, customer_count: 0, boats: []}
+      else
+        render :json => { message: "Issue with boat parameters"}, status: 404
+      end
+    end
+
+    def index
+    end
+
+    private
+
+    def timeslot_params
+      start_time = unix_time_to_datetime(params['timeslot']['start_time'])
+      { start_time: start_time, duration: params['timeslot']['duration']}
+    end
+
+    def unix_time_to_datetime(unix_time)
+      Time.at(unix_time.to_i).to_datetime
+    end
   end
 end
