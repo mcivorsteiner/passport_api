34e61bb2b2158dbd896ccabcb19b839492fffc4c
McIvor Steiner <mcivorsteiner@gmail.com>
2014-09-23 15:00:59 -0700

    added 2 tests for boats index endpoint

--- spec/controllers/api/boats_controller_spec.rb
+++ spec/controllers/api/boats_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Api::BoatsController do
-  context "create" do
+  context "POST /api/boats endpoint" do
     it "responds successfully with correct params" do
       post :create, new_boat_params
       expect(response).to be_success
@@ -11,7 +11,6 @@ describe Api::BoatsController do
       post :create, new_boat_params
 
       json = JSON.parse(response.body)
-
       expect(json.keys.length).to eq(3)
       expect(json["id"]).to be_a_kind_of(Integer)
       expect(json["capacity"]).to eq(12)
@@ -22,6 +21,23 @@ describe Api::BoatsController do
       expect {
         post :create, new_boat_params
       }.to change { Boat.count }.by(1)
+    end
+  end
+
+  context "GET /api/boats endpoint" do
+    it "responds succesfully" do
+      get :index
+      expect(response).to be_success
+    end
+
+    it "returns all existing boats" do
+      3.times { post :create, new_boat_params }
+
+      get :index
+
+      json = JSON.parse(response.body)
+      expect(json).to be_a_kind_of(Array)
+      expect(json.length).to eq(3)
     end
   end
 end
