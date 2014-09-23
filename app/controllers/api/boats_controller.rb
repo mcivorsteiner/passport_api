f7691263f27ee871442d7d13046983a7c09d4812
McIvor Steiner <mcivorsteiner@gmail.com>
2014-09-22 21:51:22 -0700

    added logic for the boats controller create method

--- app/controllers/api/boats_controller.rb
+++ app/controllers/api/boats_controller.rb
@@ -1,9 +1,21 @@
 module Api 
   class BoatsController < ApplicationController
     def create
+      @boat = Boat.new(boats_params)
+      if @boat.save
+        render :json => { id: @boat.id, capacity: @boat.capacity, name: @boat.name }
+      else
+        render :json => { message: "Issue with boat parameters"}, status: 404
+      end
     end
 
     def index
+    end
+
+    private
+
+    def boats_params
+      params['boat'].permit(:capacity, :name)
     end
   end
 end
