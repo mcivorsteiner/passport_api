d55021b44df4a59584ba9a9430d57aa73e08cf78
McIvor Steiner <mcivorsteiner@gmail.com>
2014-09-23 14:50:23 -0700

    added Test Helpers module and refactored existing tests

--- spec/controllers/api/boats_controller_spec.rb
+++ spec/controllers/api/boats_controller_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 describe Api::BoatsController do
   context "create" do
     it "responds successfully with correct params" do
-      params = { boat: { name: "Amazon Express", capacity: 12 }}
-      post :create, params
+      post :create, new_boat_params
       expect(response).to be_success
     end
 
     it "responds with correct json structure and content" do
-      params = { boat: { name: "Amazon Express", capacity: 12 }}
-      post :create, params
+      post :create, new_boat_params
+
       json = JSON.parse(response.body)
+
       expect(json.keys.length).to eq(3)
       expect(json["id"]).to be_a_kind_of(Integer)
       expect(json["capacity"]).to eq(12)
---
+++ spec/support/test_helpers.rb
@@ -0,0 +1,5 @@
+module TestHelpers
+  def new_boat_params
+    { boat: { name: "Amazon Express", capacity: 12 }}
+  end
+end
