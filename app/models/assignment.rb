d71b44051962bb385388ba0648b89f0e112d955e
McIvor Steiner <mcivorsteiner@gmail.com>
2014-09-24 01:08:03 -0700

    added long query to determine if overlapping assignment with same boat had a booking

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,6 +4,16 @@ class Assignment < ActiveRecord::Base
   has_many :bookings
 
   def availability
-    boat.capacity - bookings.sum(:group_size)
+    boat_available? ? (boat.capacity - bookings.sum(:group_size)) : 0
+  end
+
+  def boat_overlapping_assignments
+    Assignment.joins('LEFT JOIN timeslots ON timeslots.id = assignments.timeslot_id RIGHT JOIN bookings ON assignments.id = bookings.assignment_id')
+              .where( "(assignments.boat_id = ?) AND ((timeslots.start_time BETWEEN ? AND ? OR timeslots.end_time BETWEEN ? AND ?) OR (timeslots.start_time <= ? AND timeslots.end_time >= ?))", 
+                                          boat_id, timeslot.start_time, timeslot.end_time, timeslot.start_time, timeslot.end_time, timeslot.start_time, timeslot.end_time)
+  end
+
+  def boat_available?
+    boat_overlapping_assignments.empty?
   end
 end
