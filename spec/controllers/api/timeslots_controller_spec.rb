e9099e7e21a9cc2af6b20f1c47b0a9cbe60129ee
McIvor Steiner <mcivorsteiner@gmail.com>
2014-09-23 17:36:15 -0700

    added 2 tests for GET /api/timeslots endpoint

--- spec/controllers/api/timeslots_controller_spec.rb
+++ spec/controllers/api/timeslots_controller_spec.rb
@@ -13,6 +13,32 @@ describe Api::TimeslotsController do
       }.to change { Timeslot.count }.by(1)
     end
 
-    
+    it "has correct json response" do
+      post :create, new_timeslot_params
+
+      json = JSON.parse(response.body)
+      expect(json.keys.length).to eq(6)
+      expect(json["id"]).to be_a_kind_of(Integer)
+      expect(json["start_time"].to_s).to eq(new_timeslot_params['timeslot']['start_time'])
+      expect(json["duration"].to_s).to eq(new_timeslot_params['timeslot']['duration'])
+    end
+
+  end
+
+  context "GET /api/timeslots endpoint" do
+    it "responds successfully with correct params" do
+      get :index, 'date' => '2014-10-10'
+      expect(response).to be_success
+    end
+
+    it "returns all timeslots on day sent in params" do
+      3.times { post :create, new_timeslot_params }
+
+      get :index, 'date' => '2014-09-23'
+      
+      json = JSON.parse(response.body)
+      expect(json).to be_a_kind_of(Array)
+      expect(json.length).to eq(3)
+    end
   end
 end
